f258990eef807701589621faf3f125b67e8ad974
Dirk Günther <d.guenther@cenit.de>
2016-04-22 23:47:00 +0200

    Login hinzugefügt

--- src/main/groovy/de/drtodolittle/client/DrToDoLittleClient.groovy
+++ src/main/groovy/de/drtodolittle/client/DrToDoLittleClient.groovy
@@ -6,8 +6,18 @@ import static groovyx.net.http.ContentType.*
 
 public class DrToDoLittleClient {
 
-	def drtodolittle = new RestClient()
-
+	def drtodolittle = new RESTClient( 'http://www.drtodolittle.de' )
+	def token
+	
+	public void login(def username, def password) {
+		def response = drtodolittle.post( path : '/api/todos/login',
+			contentType: JSON,
+			requestContentType:  JSON,
+			body: [email: username, password: password])
+		if (response.status == 200) {
+			token = response.data.token
+		}
+	}
 
 	public void deleteToDo(String id) {
 		def response = drtodolittle.delete( path : "/api/todos/$id",
